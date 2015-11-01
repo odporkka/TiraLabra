eeee01d54e3410cfa77f1328c391318985f4df8e
Otto Porkka <ode.porkka@gmail.com>
2015-11-01 19:26:28 +0200

    Added main-class

---
+++ TiraLabra/src/main/java/tiralabra/datacompressor/appfeatures/DataCompressor.java
@@ -0,0 +1,18 @@
+package tiralabra.datacompressor.appfeatures;
+
+/**
+* @author       Otto Porkka <otto.porkka@helsinki.fi>
+* Data compressor/decompresser made as school project in fall 2015
+* Course = Data structures and algorithms lab exercise 
+* 
+*/
+
+public class DataCompressor {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Data compressing and decompressing program by Ode. v1.0");
+        
+    }
+    
+}
